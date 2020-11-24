0099e57a1c321a0e16a2defcf0f95c169d026827
PILI <PILI@macbook-air-de-m.local>
2020-11-24 11:39:46 -0300

    Initial Commit

---
+++ PracticaSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticaSwift
+//
+//  Created by PILI on 24/11/20.
+//  Copyright © 2020 PILI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
